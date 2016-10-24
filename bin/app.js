25c5b4d541b665de7a9b64dfb32de068143ecb69
lilezek <devezek@gmail.com>
2016-10-24 20:01:25 +0200

    First version.

---
+++ bin/app.js
@@ -0,0 +1,39 @@
+"use strict";
+const fs = require("fs");
+const path = require("path");
+const spawn = require('child_process');
+if (process.argv.length <= 2) {
+    console.log("Usage: " + process.argv[0] + " " + process.argv[1] + " <documentation folder>");
+    console.log("Note: docs/ folder will be path and overwrite.");
+}
+var dir = path.join(process.cwd(), process.argv[2]);
+function recursiveFix(source) {
+    if (fs.existsSync(source) && fs.lstatSync(source).isDirectory()) {
+        var files = fs.readdirSync(source);
+        files.forEach((x) => recursiveFix(source + "/" + x));
+    }
+    else {
+        if (/\.html$/i.test(source) || /\.js$/i.test(source)) {
+            var content = fs.readFileSync(source);
+            var sc = content.toString();
+            sc = sc.replace(/modules\/_([^"]+).html/ig, function (match, group) {
+                console.log(source);
+                console.log(match);
+                return "modules/" + group + ".html";
+            }).replace(/href="_([^"])+.html/ig, function (match, group) {
+                console.log(source);
+                return "href=\"" + group + ".html";
+            });
+            fs.writeFileSync(source, sc);
+            var newname = source.replace(/\/_/, "/");
+            spawn.spawn('mv', [source, newname]);
+        }
+    }
+}
+var rm = spawn.spawn('rm', ['-rf', 'docs/'])
+    .on('close', () => {
+    var cp = spawn.spawn('cp', ['-r', dir, 'docs/']);
+    cp.on('close', () => {
+        recursiveFix('docs/');
+    });
+});
